77569a3cd860249af35c7608141bd26b11567cb4
Matheus <matheusuky@gmail.com>
2026-08-29 13:17:42 -0300

    association: send packet and unify timeout

--- src/association.c
+++ src/association.c
@@ -3,10 +3,12 @@
 #include <u80211/status.h>
 
 #define AUTH_TIMEOUT 5000
+#define ASSOC_TIMEOUT 5000
 
 typedef struct {
 	void *completion_work;
 	void *auth_timeout_work;
+	void *assoc_timeout_work;
 	unsigned int generation;
 	unsigned int timeouts_in_flight;
 	u80211_device_t *device;
@@ -16,6 +18,7 @@ typedef struct {
 static void destroy_association_context(u80211_association_context_t *association_context) {
 	u80211_kernel_free_work(association_context->completion_work);
 	u80211_kernel_free_work(association_context->auth_timeout_work);
+	u80211_kernel_free_work(association_context->assoc_timeout_work);
 	u80211_kernel_free(association_context);
 }
 
@@ -25,11 +28,46 @@ void u80211_association_process_response(u80211_device_t *device, u80211_associa
 	// TODO: handle the association response
 }
 
+static void association_timeout(void *ctx, int expected_state) {
+	u80211_association_context_t *association_context = ctx;
+	u80211_device_t *device = association_context->device;
+	bool free = true;
+
+	u80211_kernel_acquire_spinlock(device->association_spinlock);
+
+	// not associating/different association generation
+	if (!device->ap || device->association_generation != association_context->generation)
+		goto leave;
+
+	if (u80211_set_device_state(device, expected_state, U80211_DEVICE_STATE_DOWN)) {
+		device->ap = NULL;
+		device->association_context = NULL;
+		++device->association_generation;
+		// TODO: wake up waiters
+	} else {
+		// already progressed
+		free = false;
+	}
+
+leave:
+	u80211_kernel_release_spinlock(device->association_spinlock);
+	if (__atomic_sub_fetch(&association_context->timeouts_in_flight, 1, __ATOMIC_ACQ_REL) == 0 && free) {
+		u80211_ap_release(association_context->ap);
+		destroy_association_context(association_context);
+	}
+}
+
+static void assoc_timeout(void *ctx) {
+	association_timeout(ctx, U80211_DEVICE_STATE_ASSOCIATING);
+}
+
 static void auth_completion_work(void *ctx) {
 	u80211_association_context_t *association_context = ctx;
 	u80211_device_t *device = association_context->device;
 
-	// TODO rest of the owl
+	u80211_send_association_request(device);
+	__atomic_add_fetch(&association_context->timeouts_in_flight, 1, __ATOMIC_RELAXED);
+	u80211_kernel_enqueue_work(association_context->assoc_timeout_work, assoc_timeout, association_context, ASSOC_TIMEOUT);
 }
 
 void u80211_association_process_authentication(u80211_device_t *device, u80211_auth_data_t *auth_data) {
@@ -65,32 +103,7 @@ leave:
 }
 
 static void auth_timeout(void *ctx) {
-	u80211_association_context_t *association_context = ctx;
-	u80211_device_t *device = association_context->device;
-	bool free = true;
-
-	u80211_kernel_acquire_spinlock(device->association_spinlock);
-
-	// not associating/different association generation
-	u80211_ap_t *ap_release = NULL;
-	if (!device->ap || device->association_generation != association_context->generation)
-		goto leave;
-
-	if (u80211_set_device_state(device, U80211_DEVICE_STATE_AUTHENTICATING, U80211_DEVICE_STATE_DOWN)) {
-		device->ap = NULL;
-		++device->association_generation;
-		// TODO: wake up waiters
-	} else {
-		// already progressed
-		free = false;
-	}
-
-leave:
-	u80211_kernel_release_spinlock(device->association_spinlock);
-	if (__atomic_sub_fetch(&association_context->timeouts_in_flight, 1, __ATOMIC_ACQ_REL) == 0 && free) {
-		u80211_ap_release(association_context->ap);
-		destroy_association_context(association_context);
-	}
+	association_timeout(ctx, U80211_DEVICE_STATE_AUTHENTICATING);
 }
 
 int u80211_associate(u80211_device_t *device, u80211_ap_t *ap) {
@@ -106,6 +119,14 @@ int u80211_associate(u80211_device_t *device, u80211_ap_t *ap) {
 
 	association_context->auth_timeout_work = u80211_kernel_allocate_work();
 	if (association_context->auth_timeout_work == NULL) {
+		u80211_kernel_free_work(association_context->completion_work);
+		u80211_kernel_free(association_context);
+		return U80211_STATUS_ENOMEM;
+	}
+
+	association_context->assoc_timeout_work = u80211_kernel_allocate_work();
+	if (association_context->assoc_timeout_work == NULL) {
+		u80211_kernel_free_work(association_context->auth_timeout_work);
 		u80211_kernel_free_work(association_context->completion_work);
 		u80211_kernel_free(association_context);
 		return U80211_STATUS_ENOMEM;
