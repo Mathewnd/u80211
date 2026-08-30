4950ebeeaf4d64036013761bd5e34674427b8bdf
Matheus <matheusuky@gmail.com>
2026-08-30 13:42:16 -0300

    string: u80211_memcmp

--- include/u80211/string.h
+++ include/u80211/string.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 
 void *u80211_memcpy(void *destination, const void *source, size_t size);
+int u80211_memcmp(const void *a, const void *b, size_t size);
 void *u80211_memset(void *destination, int value, size_t size);
 
 #endif
--- src/string.c
+++ src/string.c
@@ -11,6 +11,18 @@ void *u80211_memcpy(void *destination, const void *source, size_t size) {
 	return destination;
 }
 
+int u80211_memcmp(const void *a, const void *b, size_t size) {
+	const uint8_t *a_bytes = a;
+	const uint8_t *b_bytes = b;
+
+	for (size_t i = 0; i < size; ++i) {
+		if (a_bytes[i] != b_bytes[i])
+			return a_bytes[i] < b_bytes[i] ? -1 : 1;
+	}
+
+	return 0;
+}
+
 void *u80211_memset(void *destination, int value, size_t size) {
 	uint8_t *destination_bytes = destination;
 
